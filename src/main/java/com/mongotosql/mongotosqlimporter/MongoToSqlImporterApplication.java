80d1397ef4e32e5a1dc991730ee61cb015f34b0d
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-04-19 13:31:03 +0200

    Start structure 1.0

---
+++ src/main/java/com/mongotosql/mongotosqlimporter/MongoToSqlImporterApplication.java
@@ -0,0 +1,13 @@
+package com.mongotosql.mongotosqlimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoToSqlImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoToSqlImporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mongotosql/mongotosqlimporter/MongoToSqlImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongotosql.mongotosqlimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoToSqlImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
